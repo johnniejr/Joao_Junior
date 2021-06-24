be6191b83d1c8f576f0c56c54752b35f7ef62580
fafin <fafin@DESKTOP-GBHUVFH>
2021-06-24 13:17:31 -0300

    Nova classe e mudança no main

---
+++ Gerencia_Mudancas_git/src/git/Teste_Class3.java
@@ -0,0 +1,11 @@
+package git;
+
+public class Teste_Class3 
+{
+	String message = "Essa classe foi criada após o commit";
+			
+	public String getMessage()
+	{
+		return message;
+	}
+}
--- Gerencia_Mudancas_git/src/git/Teste_Git.java
+++ Gerencia_Mudancas_git/src/git/Teste_Git.java
@@ -9,7 +9,11 @@ public class Teste_Git
 		
 		Teste_Git2 objClassDois = new Teste_Git2();
 		
+		Teste_Class3 objClasstres = new Teste_Class3();
+		
 		System.out.println(objClassDois.getMessage());
+		
+		System.out.println(objClasstres.getMessage());
 
 	}
 
