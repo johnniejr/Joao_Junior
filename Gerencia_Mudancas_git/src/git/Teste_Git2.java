bfbc4e53b0d2f91d1246d693db371622519fe5ce
fafin <fafin@DESKTOP-GBHUVFH>
2021-06-24 13:15:32 -0300

    Terceiro teste

--- Gerencia_Mudancas_git/src/git/Teste_Git2.java
+++ Gerencia_Mudancas_git/src/git/Teste_Git2.java
@@ -2,7 +2,7 @@ package git;
 
 public class Teste_Git2 
 {
-	String message = "Esta é o segundo teste da classe Teste_Git2";
+	String message = "Esta é o terceiro teste da classe Teste_Git2";
 	
 	
 	public String getMessage()
