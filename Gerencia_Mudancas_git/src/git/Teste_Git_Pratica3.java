1e9af311da8587e687ade37fa1cf779958235172
fafin <fafin@DESKTOP-GBHUVFH>
2021-07-06 15:09:59 -0300

    Commits da prática 3

---
+++ Gerencia_Mudancas_git/src/git/Teste_Git_Pratica3.java
@@ -0,0 +1,13 @@
+package git;
+
+public class Teste_Git_Pratica3 {
+
+	public static void main(String[] args) {
+		
+		Teste_Class3 novo = new Teste_Class3();
+		
+		System.out.println(novo.getMessage2());
+
+	}
+
+}
