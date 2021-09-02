ba5b8e6664c2b93be42ae87cfbfc64559fc99e45
João Santos <johnnie-jr@hotmail.com>
2021-09-02 18:22:21 -0300

    modificacao na branch hotfix

--- Gerencia_Mudancas_git/src/git/Teste_Git.java
+++ Gerencia_Mudancas_git/src/git/Teste_Git.java
@@ -17,6 +17,8 @@ public class Teste_Git
 		
 		System.out.println(objClasstres.getMessage2());
 		
+		System.out.println("nessa branch não foi nada modificado");
+		
 		System.out.println("Exibe uma última nova mensagem");
 
 	}
